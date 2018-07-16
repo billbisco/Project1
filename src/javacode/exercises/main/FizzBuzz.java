0b19ac13392cf911d52ad30dc0903df4722232d6
Bill Bisco <billbisco@gmail.com>
2018-07-16 03:10:23 -0400

    Completed main FizzBuzz logic and tests

--- src/javacode/exercises/main/FizzBuzz.java
+++ src/javacode/exercises/main/FizzBuzz.java
@@ -1,7 +1,10 @@
 package javacode.exercises.main;
 
 public class FizzBuzz {
-    public String fizz(Integer inputNumber) {
-        return "1";
+    public String input(Integer inputNumber) {
+        if(inputNumber % 15 == 0) return "fizzbuzz";
+        if(inputNumber % 3 == 0) return "fizz";
+        if(inputNumber % 5 == 0) return "buzz";
+        else return inputNumber.toString();
     }
 }
--- src/javacode/exercises/test/FizzBuzzTest.java
+++ src/javacode/exercises/test/FizzBuzzTest.java
@@ -9,8 +9,55 @@ public class FizzBuzzTest {
     @Test
     public void whenFizzBuzzIsPassedAOneItReturnsOne() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        Assert.assertEquals("1", fizzBuzz.fizz(1));
+        Assert.assertEquals("1", fizzBuzz.input(1));
+    }
 
+    @Test
+    public void whenFizzBuzzIsPassedATwoItReturnsTwo() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        Assert.assertEquals("2", fizzBuzz.input(2));
+    }
+
+    @Test
+    public void whenFizzBuzzIsPassedAThreeItReturnsFizz() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        Assert.assertEquals("fizz", fizzBuzz.input(3));
+    }
+
+    @Test
+    public void whenFizzBuzzIsPassedAFiveItReturnsBuzz() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        Assert.assertEquals("buzz", fizzBuzz.input(5));
+    }
+
+    @Test
+    public void whenFizzBuzzIsPassedAFixItReturnsFizz() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        Assert.assertEquals("fizz", fizzBuzz.input(6));
+    }
+
+    @Test
+    public void whenFizzBuzzIsPassedATenItReturnsBuzz() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        Assert.assertEquals("buzz", fizzBuzz.input(10));
+    }
+
+    @Test
+    public void whenFizzBuzzIsPassedAFifteenItReturnsFizzBuzz() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        Assert.assertEquals("fizzbuzz", fizzBuzz.input(15));
+    }
+
+    @Test
+    public void whenFizzBuzzIsPassedAThirtyItReturnsFizzBuzz() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        Assert.assertEquals("fizzbuzz", fizzBuzz.input(30));
+    }
+
+    @Test
+    public void whenFizzBuzzIsPassedAThiirtyOneItReturnsThirtyOne() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        Assert.assertEquals("31", fizzBuzz.input(31));
     }
 
 }
