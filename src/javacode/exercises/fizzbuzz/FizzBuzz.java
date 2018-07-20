c22b3e92fef8e6b477bf2d71d9c5bbc823a3e38b
Bill Bisco <billbisco@gmail.com>
2018-07-19 22:50:35 -0400

    Fixed issue on NoGradle Branch and made it so that one of the code functions works

--- src/javacode/exercises/fizzbuzz/FizzBuzz.java
+++ src/javacode/exercises/fizzbuzz/FizzBuzz.java
@@ -1,17 +1,17 @@
 package javacode.exercises.fizzbuzz;
 
 public class FizzBuzz {
-    public String input(int inputNumber) {
+    public String input(Integer inputNumber) {
+        String value;
         if(inputNumber % 15 == 0)
-            return fizzbuzz();
+            return "fizzbuzz";
         if(inputNumber % 3 == 0)
             return fizz();
         if(inputNumber % 5 == 0)
-            return buzz();
+            return "buzz";
         else
-            return inputNumber(inputNumber);
+            return inputNumber.toString();
     }
-
     public String fizzbuzz() {
         System.out.println("fizzbuzz");
         return "fizzbuzz";
@@ -19,7 +19,8 @@ public class FizzBuzz {
 
     public String fizz() {
         System.out.println("fizz");
-        return "fizz";
+        String fizz = "fizz";
+        return fizz;
     }
 
     public String buzz() {
@@ -27,9 +28,9 @@ public class FizzBuzz {
         return "buzz";
     }
 
-    public String inputNumber(Integer inputNumber) {
-        System.out.println(inputNumber);
-        return inputNumber.toString();
-    }
+//    public String inputNumber(Integer inputNumber) {
+//        System.out.println(inputNumber);
+//        return inputNumber.toString();
+//    }
 
 }
