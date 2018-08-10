de09ed06e1d39d479424340fd728c6eaa7194a47
Bill Bisco <billbisco@gmail.com>
2018-08-10 08:12:35 -0400

    Added more classes for learning

---
+++ src/javacode/exercises/JavaLearnings/MyNumberRunner.java
@@ -0,0 +1,20 @@
+package javacode.exercises.JavaLearnings;
+
+public class MyNumberRunner {
+
+    public static void main(String[] args) {
+        MyNumber number = new MyNumber(1);
+
+        boolean isPrime = number.isPrime();
+        System.out.println("isPrime " + isPrime);
+
+        int sum = number.sumUptoN();
+        System.out.println("sumUptoN " + sum);
+
+        int sumOfDivisors = number.sumOfDivisors();
+        System.out.println("sumOfDivisors " + sumOfDivisors);
+
+        number.printNumberTriangle();
+    }
+
+}
